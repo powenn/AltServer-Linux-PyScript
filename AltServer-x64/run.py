5ba9857aea1bebe0558cd01f86c6df129016878b
powenn <powen0966091716@gmail.com>
2022-02-10 15:17:54 +0800

    restored

---
+++ AltServer-x64/run.py
@@ -0,0 +1,110 @@
+# AltServer-Linux Script python edition
+# Author of the script : powen
+
+# import 
+import os
+import subprocess
+# Check source and permission
+DIRPATH=os.path.dirname(os.path.abspath(__file__))
+os.chdir(DIRPATH)
+
+print("Checking source")
+if not os.path.exists('AltStore.ipa'):
+    subprocess.run("wget https://github.com/powenn/AltServer-Linux-PyScript/raw/main/AltStore.ipa",shell=True)
+if not os.path.exists('main.py'):
+    subprocess.run("wget https://github.com/powenn/AltServer-Linux-PyScript/raw/main/main.py",shell=True)
+if not os.path.exists('ipa'):
+    subprocess.run("mkdir ipa",shell=True)
+if not os.path.exists('saved.txt'):
+    subprocess.run("touch saved.txt",shell=True)
+if subprocess.run('stat AltServer | grep -- "-rw-r--r--"',shell=True) != "" :
+    subprocess.run("chmod +x AltServer",shell=True)
+if subprocess.run('stat AltServerDaemon | grep -- "-rw-r--r--"',shell=True) != "" :
+    subprocess.run("chmod +x AltServerDaemon",shell=True)
+
+# Version
+LocalVersion=subprocess.check_output("sed -n 1p version",shell=True).decode('utf-8').replace("\n", "")
+LatestVersion=subprocess.check_output("curl -Lsk https://github.com/powenn/AltServer-Linux-PyScript/raw/main/version",shell=True).decode('utf-8')
+
+# Help message
+HELP="""
+    
+#####################################
+#  Welcome to the AltServer script  #
+#####################################
+
+ScriptUsage: [OPTION]
+
+OPTIONS
+
+  i, --Install AltStore or ipa files
+    Install AltStore or ipa files to your device
+  d, --Restart Daemon mode
+    Restart Daemon mode to refresh apps or AltStore
+  e, --Exit
+    Exit script
+  h, --Help
+    Show this message
+  u, --Update
+    Update this script or AltServer
+
+For more information: https://github.com/powenn/AltServer-Linux-PyScript
+"""
+
+# AltServer icon
+Icon="""
+printf "\e[49m       \e[38;5;73;49m▄▄\e[38;5;66;49m▄\e[38;5;66;48;5;73m▄▄▄▄▄▄▄▄\e[38;5;66;49m▄\e[38;5;73;49m▄▄\e[49m       \e[m
+\e[49m     \e[38;5;73;49m▄\e[38;5;66;48;5;73m▄\e[38;5;66;48;5;66m▄▄▄▄▄▄\e[38;5;67;48;5;66m▄▄\e[38;5;66;48;5;66m▄▄▄▄▄▄\e[38;5;66;48;5;73m▄\e[38;5;73;49m▄\e[49m     \e[m
+\e[49m   \e[38;5;73;49m▄\e[38;5;30;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;109;48;5;73m▄\e[38;5;109;48;5;109m▄▄▄▄\e[38;5;109;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;30;48;5;73m▄\e[38;5;73;49m▄\e[49m   \e[m
+\e[49m \e[38;5;73;49m▄\e[38;5;30;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;73;48;5;73m▄▄▄▄▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;30;48;5;30m▄▄▄▄▄▄\e[38;5;30;48;5;73m▄\e[38;5;73;49m▄\e[49m \e[m
+\e[49m \e[38;5;30;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;15;48;5;73m▄\e[38;5;15;48;5;15m▄▄\e[38;5;15;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;30m▄\e[38;5;30;48;5;30m▄▄▄▄▄\e[38;5;30;48;5;73m▄\e[49m \e[m
+\e[38;5;73;48;5;73m▄\e[38;5;30;48;5;30m▄▄▄\e[38;5;73;48;5;30m▄▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;15;48;5;73m▄\e[38;5;15;48;5;15m▄\e[48;5;15m    \e[38;5;15;48;5;15m▄\e[38;5;15;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;30m▄▄\e[38;5;30;48;5;30m▄▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[38;5;73;48;5;73m▄\e[38;5;30;48;5;30m▄▄\e[38;5;66;48;5;30m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;15;48;5;73m▄\e[48;5;15m          \e[38;5;15;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;66;48;5;30m▄\e[38;5;30;48;5;30m▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[38;5;73;48;5;73m▄\e[38;5;30;48;5;30m▄▄\e[38;5;30;48;5;66m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;15m▄\e[48;5;15m          \e[38;5;73;48;5;15m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;30;48;5;66m▄\e[38;5;30;48;5;30m▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[38;5;73;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄\e[38;5;6;48;5;66m▄\e[38;5;6;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;15m▄\e[38;5;15;48;5;15m▄\e[48;5;15m    \e[38;5;15;48;5;15m▄\e[38;5;73;48;5;15m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;6;48;5;72m▄\e[38;5;6;48;5;6m▄▄▄\e[38;5;73;48;5;73m▄\e[m
+\e[49m \e[38;5;73;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;73;48;5;15m▄\e[38;5;15;48;5;15m▄▄\e[38;5;73;48;5;15m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄▄▄\e[38;5;73;48;5;6m▄\e[49m \e[m
+\e[49m \e[49;38;5;66m▀\e[38;5;67;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄▄▄▄▄\e[38;5;6;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;67;48;5;6m▄\e[49;38;5;66m▀\e[49m \e[m
+\e[49m   \e[49;38;5;67m▀\e[38;5;66;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;66;48;5;73m▄\e[38;5;73;48;5;73m▄▄▄▄\e[38;5;66;48;5;73m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;66;48;5;6m▄\e[49;38;5;67m▀\e[49m   \e[m
+\e[49m     \e[49;38;5;66m▀\e[38;5;66;48;5;6m▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;6;48;5;30m▄▄\e[38;5;6;48;5;6m▄▄▄▄▄▄\e[38;5;66;48;5;6m▄\e[49;38;5;66m▀\e[49m     \e[m
+\e[49m       \e[49;38;5;66m▀▀\e[49;38;5;6m▀\e[38;5;66;48;5;6m▄▄▄▄▄▄▄▄\e[49;38;5;6m▀\e[49;38;5;66m▀▀\e[49m       \e[m
+"
+"""
+def AltServerIcon() :
+    subprocess.run(Icon,shell=True)
+# Show update avaliable message
+def UpdateNotification() :
+    if LatestVersion != LocalVersion :
+        print("-------<< UPDATE AVALIABLE >>-------")
+
+def getAnswer(text):
+     return input(text)
+
+# Start script
+AltServerIcon()
+print(HELP)
+UpdateNotification()
+print("Please connect to your device and press Enter to continue")
+subprocess.run('read key',shell=True)
+subprocess.run('idevicepair pair > /dev/null',shell=True)
+subprocess.run('./AltServerDaemon &> /dev/null &',shell=True)
+
+RunScript=0
+while RunScript==0 :
+    option=getAnswer("Enter OPTION to continue :")
+    if option == 'i' : # Install-AltStore-or-ipa-files
+        subprocess.run('python3 main.py',shell=True)
+    if option == 'd' : # Restart-Daemon-mode
+        subprocess.run('killall AltServerDaemon',shell=True)
+        subprocess.run('idevicepair pair',shell=True)
+        subprocess.run('./AltServerDaemon &> /dev/null &',shell=True)
+    if option == 'e' : # Exit
+        subprocess.run('killall AltServerDaemon',shell=True)
+        break
+    if option == 'h' : # Help
+        AltServerIcon()
+        print(HELP)
+        UpdateNotification()
+    if option == 'u' : # Update
+        subprocess.run("curl -Lsk 'https://github.com/powenn/AltServer-Linux-PyScript/raw/main/update.py' | python3",shell=True)
+
+
